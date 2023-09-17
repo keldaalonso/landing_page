98dc353c91cbb8fd1b562319e96bbc42e4b5470e
Keldabless <keldablessenow@gmail.com>
2023-09-17 16:03:17 +0100

    home page completed

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ navItems.forEach(item => {
     });
 });
 
-// JavaScript function to toggle button styles
-function toggleButtonStyles(button) {
+// JavaScript function to toggle button styles and show/hide the form
+function toggleButtonStylesAndForm(button) {
     const buttons = document.querySelectorAll('.btn-login, .btn-register');
+    const registrationForm = document.getElementById('registration-container');
     
     buttons.forEach((btn) => {
         if (btn !== button) {
@@ -19,6 +20,13 @@ function toggleButtonStyles(button) {
     });
     
     button.classList.toggle('btn-active');
+    
+    // Toggle the visibility of the registration form
+    if (button.classList.contains('btn-active')) {
+        registrationForm.style.display = 'block';
+    } else {
+        registrationForm.style.display = 'none';
+    }
 }
 
 // Add click event listeners to the buttons
@@ -27,11 +35,30 @@ const registerButton = document.querySelector('.btn-register');
 
 if (loginButton && registerButton) {
     loginButton.addEventListener('click', function() {
-        toggleButtonStyles(this);
+        toggleButtonStylesAndForm(this);
     });
 
     registerButton.addEventListener('click', function() {
-        toggleButtonStyles(this);
+        toggleButtonStylesAndForm(this);
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownPlaceholder = document.querySelector(".dropdown-placeholder");
+    const dropdownContent = document.querySelector(".dropdown-content");
+  
+    dropdownPlaceholder.addEventListener("click", function () {
+      this.parentElement.classList.toggle("active");
+      dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+    });
+  
+    // Close the dropdown when clicking outside
+    document.addEventListener("click", function (event) {
+      if (!dropdownPlaceholder.contains(event.target)) {
+        dropdownContent.style.display = "none";
+        dropdownPlaceholder.parentElement.classList.remove("active");
+      }
+    });
+  });
+  
 
