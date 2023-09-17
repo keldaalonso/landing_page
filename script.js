10a711787ae0fa6a98fc1a845a81457d5823e0fd
Keldabless <keldablessenow@gmail.com>
2023-09-17 12:33:46 +0100

    navbar styling done

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ navItems.forEach(item => {
         this.classList.add('current');
     });
 });
+
+// JavaScript function to toggle button styles
+function toggleButtonStyles(button) {
+    const buttons = document.querySelectorAll('.btn-login, .btn-register');
+    
+    buttons.forEach((btn) => {
+        if (btn !== button) {
+            btn.classList.remove('btn-active');
+        }
+    });
+    
+    button.classList.toggle('btn-active');
+}
+
+// Add click event listeners to the buttons
+const loginButton = document.querySelector('.btn-login');
+const registerButton = document.querySelector('.btn-register');
+
+if (loginButton && registerButton) {
+    loginButton.addEventListener('click', function() {
+        toggleButtonStyles(this);
+    });
+
+    registerButton.addEventListener('click', function() {
+        toggleButtonStyles(this);
+    });
+}
+
